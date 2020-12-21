439b03d381dc3e3de174a9a85ac71935135f3f24
Dustin Whisman <whisman.dustin@gmail.com>
2020-12-21 14:34:13 -0700

    Set acceptable base styles.

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy('src/robots.txt');
-  eleventyConfig.addPassthroughCopy('src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/favicon.png');
   eleventyConfig.addPassthroughCopy('src/service-worker.js');
   eleventyConfig.addPassthroughCopy('src/manifest.json');
   eleventyConfig.addPassthroughCopy('src/icons/*');
