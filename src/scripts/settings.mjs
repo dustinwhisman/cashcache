8e4e7d764bad3ecd44cb5e9bbd5b834fa8b329c0
Dustin Whisman <whisman.dustin@gmail.com>
2020-12-24 18:57:22 -0700

    Add listener and logic for bulk uploading savings.

--- src/scripts/settings.mjs
+++ src/scripts/settings.mjs
@@ -314,7 +314,7 @@ document.addEventListener('change', (event) => {
       importProgressIndicator.innerHTML = '<p>Loading file...</p>';
       try {
         const incomeData = csvStringToArray(e.target.result);
-        importProgressIndicator.innerHTML = '<p>Uploading Expenses...</p>';
+        importProgressIndicator.innerHTML = '<p>Uploading Income...</p>';
 
         await Promise.all(incomeData.map(async (income) => {
           const date = new Date(income['Date']);
@@ -329,6 +329,51 @@ document.addEventListener('change', (event) => {
           };
 
           await addToDb('income', newIncome);
+          Promise.resolve();
+        }));
+
+        importProgressIndicator.innerHTML = `
+          <p>
+            All done! You should see all your data on the
+            <a href="/overview">overview page</a> now.
+          </p>
+        `;
+      } catch (error) {
+        importProgressIndicator.innerHTML = `
+          <p>
+            There was a problem importing data from that file. Please double
+            check the file and try again. If you continue to have problems,
+            contact us at
+            <a href="mailto:help@cashcache.io">help@cashcache.io</a>.
+          </p>
+        `;
+      }
+    };
+    reader.readAsText(file);
+  }
+
+  if (event.target.matches('[data-import-savings-data]')) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const importProgressIndicator = document.querySelector('[data-import-savings-progress]');
+      importProgressIndicator.innerHTML = '<p>Loading file...</p>';
+      try {
+        const savingsData = csvStringToArray(e.target.result);
+        importProgressIndicator.innerHTML = '<p>Uploading Savings...</p>';
+
+        await Promise.all(savingsData.map(async (savings) => {
+          const date = new Date(savings['Date']);
+          const newSavings = {
+            year: date.getFullYear(),
+            month: date.getMonth(),
+            category: savings['Category'],
+            description: savings['Description'],
+            amount: sanitize(savings['Balance']),
+            key: null,
+          };
+
+          await addToDb('savings', newSavings);
           Promise.resolve();
         }));
 
