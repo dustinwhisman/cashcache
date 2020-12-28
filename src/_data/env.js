97a112bfb78efe3b1ceef507b6b82bda1a1e0380
Dustin Whisman <whisman.dustin@gmail.com>
2020-12-28 11:38:12 -0700

    Set up firebase auth with the login/signup flow.

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
   environment: process.env.NODE_ENV,
+  firebaseApiKey: process.env.FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  firebaseAppId: process.env.FIREBASE_APP_ID,
 };
