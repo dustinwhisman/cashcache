1e39963eb6f37e5ccf910539a4c8ff9c7e2b9d92
Dustin Whisman <whisman.dustin@gmail.com>
2020-12-30 11:19:08 -0700

    Try improving mondo error response.

--- functions/get-all-from-index/get-all-from-index.js
+++ functions/get-all-from-index/get-all-from-index.js
@@ -30,7 +30,7 @@ const handler = async (event) => {
     } catch (error) {
       return {
         statusCode: 500,
-        body: JSON.stringify(error),
+        body: JSON.stringify({ ...error }),
       };
     }
 };
