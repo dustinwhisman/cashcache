2bfdf2fcca23dd3482f2d38a9c62cdefbb44ac9b
Dustin Whisman <whisman.dustin@gmail.com>
2020-12-31 10:40:21 -0700

    Add api for creating a Stripe session.

---
+++ functions/create-checkout-session/create-checkout-session.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+const handler = async (event) => {
+  const { priceId } = JSON.parse(event.body);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'subscription',
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:8888' : 'https://cashcache.io'}/payment-success?sessionId={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NODE_ENV === 'development' ? 'http://localhost:8888' : 'https://cashcache.io'}/overview`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        sessionId: session.id,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: {
+          message: error.message,
+        },
+      }),
+    };
+  }
+};
+
+module.exports = { handler };
