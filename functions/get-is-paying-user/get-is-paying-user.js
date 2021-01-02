8f18e14d2a3fce6bdcc144c0044d1ef19011cefe
Dustin Whisman <whisman.dustin@gmail.com>
2021-01-02 12:55:38 -0700

    Revert "Split get-is-paying-user into two functions to circumvent error (maybe)."
    This reverts commit be8c08150dfd793f422c5473d7d0c3f3e742d2eb.

--- functions/get-is-paying-user/get-is-paying-user.js
+++ functions/get-is-paying-user/get-is-paying-user.js
@@ -1,14 +1,50 @@
 require('dotenv').config();
 const Stripe = require('stripe');
+const MongoClient = require('mongodb').MongoClient;
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+console.log({ isDevelopment });
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+console.log({ stripe });
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
+console.log({ uri });
 
-const getIsPayingUser = async (customerId) => {
+let cachedDb = null;
+let client;
+
+const connectToDatabase = async (uri) => {
+  if (cachedDb) {
+    console.log({ cachedDb });
+    return cachedDb;
+  }
+
+  client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log({ client });
+
+  cachedDb = client.db(process.env.MONGODB_DB_NAME);
+
+  console.log({ cachedDb });
+  return cachedDb;
+};
+
+const getCustomerId = async (db, uid) => {
+  const { customerId } = await db.collection('users').findOne({ uid });
+  console.log({ customerId });
+
+  if (isDevelopment && client != null) {
+    await client.close();
+  }
+
   const customer = await stripe.customers.retrieve(customerId, {
     expand: ['subscriptions'],
   });
+  console.log({ customer });
 
   const isPayingUser = !!customer?.subscriptions?.data?.some(subscription => subscription?.status === 'active');
+  console.log({ isPayingUser });
 
   return {
     statusCode: 200,
@@ -16,10 +52,15 @@ const getIsPayingUser = async (customerId) => {
   };
 };
 
-const handler = async (event) => {
+const handler = async (event, context) => {
   try {
-    const { customerId } = JSON.parse(event.body);
-    return getIsPayingUser(customerId);
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    const { uid } = JSON.parse(event.body);
+    console.log({ uid });
+    const db = await connectToDatabase(uri);
+    console.log({ db });
+    return getCustomerId(db, uid);
   } catch (error) {
     console.log({ error });
     return {
