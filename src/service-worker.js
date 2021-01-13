d1297694f1b0a70f3bd21076fbe2fee2e4215b8a
Dustin Whisman <whisman.dustin@gmail.com>
2021-01-13 16:20:54 -0700

    Improve default service worker strategy.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,38 @@
-self.addEventListener('fetch', function(event) {
+const staleWhileRevalidate = (event) => {
   event.respondWith(
-    caches.open('dynamic').then(function(cache) {
-      return fetch(event.request).then(function(response) {
+    caches.open('dynamic').then((cache) => {
+      return cache.match(event.request).then((response) => {
+        var fetchPromise = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    }),
+  );
+};
+
+const networkUpdatingCache = (event) => {
+  event.respondWith(
+    caches.open('dynamic').then((cache) => {
+      return fetch(event.request).then((response) => {
         cache.put(event.request, response.clone());
         return response;
       });
-    })
+    }),
   );
+};
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method === 'POST') {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  if (/^\/api\//.test(requestUrl.pathname)) {
+    networkUpdatingCache(event);
+    return;
+  }
+
+  staleWhileRevalidate(event);
 });
