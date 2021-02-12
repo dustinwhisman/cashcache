cc2c4b97d46734815152839cd79a0bf019d367ec
Dustin Whisman <whisman.dustin@gmail.com>
2021-02-12 15:58:41 -0700

    Move category event logic into utility function.

--- src/scripts/utilities/index.mjs
+++ src/scripts/utilities/index.mjs
@@ -4,3 +4,19 @@ export const updateBackLink = () => {
     backLink.href = document.referrer;
   }
 }
+
+export const addCategoryEventListener = () => {
+  document.addEventListener('change', (event) => {
+    if (event.target.matches('[name=category]')) {
+      const newCategoryDiv = document.querySelector('[data-new-category]');
+      const newCategoryInput = document.querySelector('#new-category');
+      if (event.target.value === 'new-category') {
+        newCategoryDiv.removeAttribute('hidden');
+        newCategoryInput.setAttribute('required', true);
+      } else {
+        newCategoryDiv.setAttribute('hidden', true);
+        newCategoryInput.removeAttribute('required');
+      }
+    }
+  });
+};
