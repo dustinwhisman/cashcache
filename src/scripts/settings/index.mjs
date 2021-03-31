4bffe41828e2a1551c26aa07bc06624cb389f996
Dustin Whisman <whisman.dustin@gmail.com>
2021-03-31 11:41:58 -0600

    Get expenses sorting preferences working.

--- src/scripts/settings/index.mjs
+++ src/scripts/settings/index.mjs
@@ -141,36 +141,6 @@ const currentDateSpan = document.querySelector('[data-current-date]');
 const today = new Date();
 currentDateSpan.innerHTML = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
-// expenses sorting preferences
-(() => {
-  const preferences = localStorage.getItem('expenses-preferences') || '{}';
-  const {
-    groupByCategory = true,
-    sortBy = 'amount',
-    order = 'descending'
-  } = JSON.parse(preferences);
-
-  if (groupByCategory) {
-    const groupByCategoryInput = document.querySelector('[name=group-expenses-by-category]');
-    groupByCategoryInput.checked = true;
-  }
-
-  const sortByInput = document.querySelector(`[name=sort-expenses-by][value=${sortBy}]`);
-  const orderInput = document.querySelector(`[name=expenses-sort-order][value=${order}]`);
-  sortByInput.checked = true;
-  orderInput.checked = true;
-
-  const descendingLabel = document.querySelector('[data-expenses-descending-label]');
-  const ascendingLabel = document.querySelector('[data-expenses-ascending-label]');
-  if (sortBy === 'amount') {
-    descendingLabel.innerHTML = 'Largest to Smallest';
-    ascendingLabel.innerHTML = 'Smallest to Largest';
-  } else {
-    descendingLabel.innerHTML = 'Newest to Oldest';
-    ascendingLabel.innerHTML = 'Oldest to Newest';
-  }
-})();
-
 // income sorting preferences
 (() => {
   const preferences = localStorage.getItem('income-preferences') || '{}';
@@ -230,23 +200,6 @@ currentDateSpan.innerHTML = `${today.getFullYear()}-${today.getMonth() + 1}-${to
 })();
 
 document.addEventListener('change', (event) => {
-  if (event.target.matches('[name=sort-expenses-by]')) {
-    const descendingLabel = document.querySelector('[data-expenses-descending-label]');
-    const ascendingLabel = document.querySelector('[data-expenses-ascending-label]');
-    if (event.target.value === 'amount') {
-      descendingLabel.innerHTML = 'Largest to Smallest';
-      ascendingLabel.innerHTML = 'Smallest to Largest';
-    } else {
-      descendingLabel.innerHTML = 'Newest to Oldest';
-      ascendingLabel.innerHTML = 'Oldest to Newest';
-    }
-  }
-
-  if (event.target.matches('[data-expenses-preferences-form] *')) {
-    const savePreferencesButton = document.querySelector('[data-save-expenses-preferences]');
-    savePreferencesButton.innerHTML = 'Save Preferences';
-  }
-
   if (event.target.matches('[name=sort-income-by]')) {
     const descendingLabel = document.querySelector('[data-income-descending-label]');
     const ascendingLabel = document.querySelector('[data-income-ascending-label]');
@@ -540,19 +493,6 @@ document.addEventListener('change', (event) => {
 
 document.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (event.target.matches('[data-expenses-preferences-form]')) {
-    const { elements } = event.target;
-    const preferences = {
-      groupByCategory: elements['group-expenses-by-category'].checked,
-      sortBy: elements['sort-expenses-by'].value,
-      order: elements['expenses-sort-order'].value,
-    };
-
-    localStorage.setItem('expenses-preferences', JSON.stringify(preferences));
-    const savePreferencesButton = document.querySelector('[data-save-expenses-preferences]');
-    savePreferencesButton.innerHTML = 'Saved!';
-  }
-
   if (event.target.matches('[data-income-preferences-form]')) {
     const { elements } = event.target;
     const preferences = {
