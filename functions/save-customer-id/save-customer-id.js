73526ab20aade1442da24abf0f7a9b1dc0625224
Dustin Whisman <whisman.dustin@gmail.com>
2023-10-28 09:57:06 -0600

    chore: update dependencies (#21)
    * chore: update minor version dependencies
    
    * chore: update postcss-preset-env
    
    * chore: update stripe to its latest version
    
    * chore: update mongodb driver to latest version
    
    * chore: configure dependabot for weekly updates

--- functions/save-customer-id/save-customer-id.js
+++ functions/save-customer-id/save-customer-id.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const { initializeApp, cert, getApps, getApp } = require('firebase-admin/app');
-const { getAuth } = require('firebase-admin/auth');
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const { initializeApp, cert, getApps, getApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 let cachedDb = null;
@@ -15,10 +15,7 @@ const connectToDatabase = async (uri) => {
     return cachedDb;
   }
 
-  client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  client = await MongoClient.connect(uri);
 
   cachedDb = client.db(process.env.MONGODB_DB_NAME);
 
@@ -29,7 +26,7 @@ const saveCustomerId = async (db, uid, customerId) => {
   const query = { uid };
   const update = { $set: { customerId } };
   const options = { upsert: true };
-  await db.collection('users').updateOne(query, update, options);
+  await db.collection("users").updateOne(query, update, options);
 
   if (isDevelopment && client != null) {
     await client.close();
@@ -37,7 +34,7 @@ const saveCustomerId = async (db, uid, customerId) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Customer ID updated successfully.' }),
+    body: JSON.stringify({ message: "Customer ID updated successfully." }),
   };
 };
 
@@ -54,14 +51,14 @@ const handler = async (event, context) => {
 
     let uid;
     let token = event.headers.authorization;
-    token = token.replace(/^Bearer\s+/, '');
+    token = token.replace(/^Bearer\s+/, "");
     if (token) {
       const decodedToken = await getAuth(app).verifyIdToken(token);
       uid = decodedToken.uid;
     } else {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: 'This request was not authorized.' }),
+        body: JSON.stringify({ message: "This request was not authorized." }),
       };
     }
 
