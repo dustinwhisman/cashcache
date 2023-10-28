ba2c1e4fb1e354e7dd6a39cc0ec5bc3deeb61c7f
Dustin Whisman <whisman.dustin@gmail.com>
2023-10-28 09:47:16 -0600

    chore: update mongodb driver to latest version

--- functions/delete-account/delete-account.js
+++ functions/delete-account/delete-account.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const Stripe = require('stripe');
-const MongoClient = require('mongodb').MongoClient;
-const { initializeApp, cert, getApps, getApp } = require('firebase-admin/app');
-const { getAuth } = require('firebase-admin/auth');
+require("dotenv").config();
+const Stripe = require("stripe");
+const MongoClient = require("mongodb").MongoClient;
+const { initializeApp, cert, getApps, getApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
@@ -17,10 +17,7 @@ const connectToDatabase = async (uri) => {
     return cachedDb;
   }
 
-  client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  client = await MongoClient.connect(uri);
 
   cachedDb = client.db(process.env.MONGODB_DB_NAME);
 
@@ -28,22 +25,24 @@ const connectToDatabase = async (uri) => {
 };
 
 const deleteAccount = async (db, uid) => {
-  const { customerId } = await db.collection('users').findOne({ uid });
-  await db.collection('users').deleteOne({ uid });
+  const { customerId } = await db.collection("users").findOne({ uid });
+  await db.collection("users").deleteOne({ uid });
 
   if (isDevelopment && client != null) {
     await client.close();
   }
 
   const customer = await stripe.customers.retrieve(customerId, {
-    expand: ['subscriptions'],
+    expand: ["subscriptions"],
   });
 
-  await Promise.all(customer.subscriptions.data.map(async (subscription) => {
-    await stripe.subscriptions.del(subscription.id);
+  await Promise.all(
+    customer.subscriptions.data.map(async (subscription) => {
+      await stripe.subscriptions.del(subscription.id);
 
-    return Promise.resolve();
-  }));
+      return Promise.resolve();
+    })
+  );
 
   const deleted = await stripe.customers.del(customerId);
 
@@ -66,14 +65,14 @@ const handler = async (event, context) => {
 
     let uid;
     let token = event.headers.authorization;
-    token = token.replace(/^Bearer\s+/, '');
+    token = token.replace(/^Bearer\s+/, "");
     if (token) {
       const decodedToken = await getAuth(app).verifyIdToken(token);
       uid = decodedToken.uid;
     } else {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: 'This request was not authorized.' }),
+        body: JSON.stringify({ message: "This request was not authorized." }),
       };
     }
 
--- functions/get-is-paying-user/get-is-paying-user.js
+++ functions/get-is-paying-user/get-is-paying-user.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const Stripe = require('stripe');
-const MongoClient = require('mongodb').MongoClient;
-const { initializeApp, cert, getApps, getApp } = require('firebase-admin/app');
-const { getAuth } = require('firebase-admin/auth');
+require("dotenv").config();
+const Stripe = require("stripe");
+const MongoClient = require("mongodb").MongoClient;
+const { initializeApp, cert, getApps, getApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
@@ -17,10 +17,7 @@ const connectToDatabase = async (uri) => {
     return cachedDb;
   }
 
-  client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  client = await MongoClient.connect(uri);
 
   cachedDb = client.db(process.env.MONGODB_DB_NAME);
 
@@ -28,7 +25,7 @@ const connectToDatabase = async (uri) => {
 };
 
 const getCustomerId = async (db, uid) => {
-  const { customerId } = await db.collection('users').findOne({ uid });
+  const { customerId } = await db.collection("users").findOne({ uid });
 
   if (isDevelopment && client != null) {
     await client.close();
@@ -42,10 +39,12 @@ const getCustomerId = async (db, uid) => {
   }
 
   const customer = await stripe.customers.retrieve(customerId, {
-    expand: ['subscriptions'],
+    expand: ["subscriptions"],
   });
 
-  const isPayingUser = customer.subscriptions.data.some(subscription => subscription.status === 'active');
+  const isPayingUser = customer.subscriptions.data.some(
+    (subscription) => subscription.status === "active"
+  );
 
   return {
     statusCode: 200,
@@ -66,14 +65,14 @@ const handler = async (event, context) => {
 
     let uid;
     let token = event.headers.authorization;
-    token = token.replace(/^Bearer\s+/, '');
+    token = token.replace(/^Bearer\s+/, "");
     if (token) {
       const decodedToken = await getAuth(app).verifyIdToken(token);
       uid = decodedToken.uid;
     } else {
       return {
         statusCode: 401,
-        body: JSON.stringify({ message: 'This request was not authorized.' }),
+        body: JSON.stringify({ message: "This request was not authorized." }),
       };
     }
 
